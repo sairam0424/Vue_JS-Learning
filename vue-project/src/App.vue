9debc91a5e02afa428ed886e09b0723b664dca12
sairam0424 <uggesairam0000@gmail.com>
2024-09-30 15:18:59 +0530

    Feat(Frontend):Added the inc and dec counter by passing the arguments to the function added the inc , dec , inc by num  ,  dec by num to the application and implemented using the vue composition api

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -8,11 +8,32 @@ function increment(){
   count.value++;
 }
 
+function decrement(){
+  count.value--;
+}
+ function increment_number(number){
+
+    count.value+=number;
+
+ }
+
+ function decrement_number(number){
+   count.value-=number;
+ }
+
 onMounted(()=>{
   console.log(`This is the count ${count.value}`);
 })
 </script>
 
 <template>
-<button @click="increment"> Count is {{ count }}</button>
+<button @click="increment">Inc  Count is {{ count }}</button>
+
+<button @click="decrement">Dec Count is {{ count }}</button>
+
+<button @click="increment_number(5)">Inc count Count is </button>
+
+<button @click="decrement_number(5)"> Decrement Count is </button>  
+
+<h1>{{ count }}</h1>
 </template>
