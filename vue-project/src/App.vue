3420b94fc7017f516c317916c1cd52b382ecb0bd
sairam0424 <uggesairam0000@gmail.com>
2024-09-30 15:41:08 +0530

    feat(Frontend)  : Added the input field to get the name dynamically updated used the native event object to updated the name from the input field and get it displayed in the h1 field

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,6 +4,10 @@ import { ref , onMounted} from 'vue'
 
 const count = ref(0);
 
+const name = ref('');
+
+const lastName = ref("Asc")
+
 function increment(){
   count.value++;
 }
@@ -20,6 +24,9 @@ function decrement(){
  function decrement_number(number){
    count.value-=number;
  }
+function updateName(event , lastName){
+   name.value = event.target.value + lastName;
+}
 
 onMounted(()=>{
   console.log(`This is the count ${count.value}`);
@@ -33,7 +40,16 @@ onMounted(()=>{
 
 <button @click="increment_number(5)">Inc count Count is </button>
 
-<button @click="decrement_number(5)"> Decrement Count is </button>  
+<button @click="decrement_number(5)"> Decrement Count is </button> 
+<br> 
+<br>
+
+
+<input type="text" @input="updateName($event , lastName)" placeholder="Give me the name"></input>
+
+<h1>{{ name }}</h1>
+
+<br>
 
 <h1>{{ count }}</h1>
 </template>
